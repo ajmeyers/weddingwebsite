01bf2c5089791cf7940408a3a02b3b10587927c7
Andrew Meyers <ameyers.central@gmail.com>
2019-11-20 16:04:21 -0500

    Updating Frontend

--- wedding-website/src/App.js
+++ wedding-website/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Engaged from "./engaged.JPG";
+// import Background from "./background.jpg";
+import Countdown from 'react-countdown-now';
+
+
 import './App.css';
 
+
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
+    
+      <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet"></link>
+      <h1>Giselle and Andrew</h1>
+      <img src = {Engaged} alt="Engaged" width="525" height="400"/>
+      <h1>Countdown until our Wedding Day!</h1>
+      <Countdown date={Date.now() + 10000}/>
+
+     {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +32,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header> */ }
     </div>
   );
 }
